f5c2f027d487bee7de27f01863715023d93bf395
Benjamin <tankerkzheng1@gmail.com>
2023-10-14 03:39:49 +0800

    added default avatar & update profile component

--- components/Profileleft.js
+++ components/Profileleft.js
@@ -1,10 +1,19 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Profileleft = () => {
   return (
     <div className="top-0 left-0 w-3/12 borderr top-0 bottom-0 right-0">
-        <div>name</div>
-        somethng else
+      <div className="mt-8 mb-6 text-center">
+        <h1 className="font-bold text-2xl">Profile</h1>
+      </div>
     </div>
   );
 };
