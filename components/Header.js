ea4a255d42a757391c233377bccab97ee8991f11
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-10-13 23:41:18 +0800

    second push for UI

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,21 @@ import Image from "next/image";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 const Header = () => {
   return (
+    
     <header className="flex flex-col gap-5 border-b border-gray-350">
       <div className="py-4 flex items-center justify-between">
         <Link href="/">
