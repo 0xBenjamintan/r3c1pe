23dcc8e39c79f907a3f196ef67a2f04067c89bf6
Benjamin <tankerkzheng1@gmail.com>
2023-10-14 02:46:00 +0800

    changed layout & logo

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import dynamic from "next/dynamic";
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,13 +12,12 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
 
 const Header = () => {
   return (
-    
     <header className="flex flex-col gap-5 border-b border-gray-350">
-      <div className="py-4 flex items-center justify-between">
+      <div className="py-6 flex items-center justify-between">
         <Link href="/">
           <div className="flex items-center">
             <Image
@@ -28,7 +27,7 @@ const Header = () => {
               className="w-8 md:w-9"
               alt="logo"
             />
-            <p className="text-2xl font-bold ml-2">ETHKL</p>
+            <p className="text-2xl font-bold ml-2">R3C1PE</p>
           </div>
         </Link>
         <div className="flex">
@@ -37,11 +36,11 @@ const Header = () => {
           </Link>
           <p className="text-2xl ml-2">|</p>
           <Link href="/plans">
-          <p className="text-2xl ml-2 hover">Plan </p>
+            <p className="text-2xl ml-2 hover">Plan </p>
           </Link>
           <p className="text-2xl ml-2">|</p>
           <Link href="/profile">
-          <p className="text-2xl ml-2 hover">Profile</p>
+            <p className="text-2xl ml-2 hover">Profile</p>
           </Link>
         </div>
         <div className="flex flex-wrap justify-end">
