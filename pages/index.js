a1bbc29393fac58c28f60991284e3e30c1e3e1fa
Benjamin <tankerkzheng1@gmail.com>
2023-10-13 23:41:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Planscard from "@/components/Planscard";
+import { Head } from "react-day-picker";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      hi this is the landing page
-    </main>
-  )
+    <>
+      <Head>EthKL Project</Head>
+    </>
+  );
 }
