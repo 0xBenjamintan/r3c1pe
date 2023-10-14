88692afddb6169fc38d9184c53d113d194a97821
Benjamin <tankerkzheng1@gmail.com>
2023-10-14 20:16:36 +0800

    Update Profileleft.js

--- components/Profileleft.js
+++ components/Profileleft.js
@@ -44,49 +44,53 @@ const Profileleft = () => {
   }, [isDisconnected, router]);
 
   return (
-    <div className="top-0 left-0 w-3/12 borderr top-0 bottom-0 right-0">
-      <Card className="w-[350px] h-[500px] mt-10 ">
-        <CardHeader className="my-6">
-          {ensAvatar ? (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              src={ensAvatar}
-              alt="avatar"
-              className="w-20 h-20 rounded-full mb-4"
-            />
-          ) : (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              src="/avatar.png"
-              alt="avatar"
-              className="w-20 h-20 rounded-full mb-4"
-            />
-          )}
-          {ensName ? (
-            <CardTitle>Wallet Address: {ensName}</CardTitle>
-          ) : (
-            <CardTitle>Wallet Address: {truncateLine(walletAdd)}</CardTitle>
-          )}
-          <CardDescription>
-            {/* replace with ens or pub key */}
-            Deploy your new project in one-click.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Package Type:</Label>
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework">Description:</Label>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between"></CardFooter>
-      </Card>
-    </div>
+    <>
+      <div className="flex w-full flex-col md:flex-row">
+        <div className="top-0 left-0 w-3/12 top-0 bottom-0 right-0">
+          <Card className="w-[350px] h-[500px] mt-10 ">
+            <CardHeader className="my-6">
+              {ensAvatar ? (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  src={ensAvatar}
+                  alt="avatar"
+                  className="w-20 h-20 rounded-full mb-4"
+                />
+              ) : (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  src="/avatar.png"
+                  alt="avatar"
+                  className="w-20 h-20 rounded-full mb-4"
+                />
+              )}
+              {ensName ? (
+                <CardTitle>Wallet Address: {ensName}</CardTitle>
+              ) : (
+                <CardTitle>Wallet Address: {truncateLine(walletAdd)}</CardTitle>
+              )}
+              <CardDescription>
+                {/* replace with ens or pub key */}
+                Deploy your new project in one-click.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Package Type:</Label>
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="framework">Description:</Label>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between"></CardFooter>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 };
 
