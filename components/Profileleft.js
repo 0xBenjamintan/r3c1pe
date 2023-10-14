33e12ee4bb1aecc6e41ac418505951c6d0cfb8a4
Benjamin <tankerkzheng1@gmail.com>
2023-10-14 19:55:41 +0800

    Update Profileleft.js

--- components/Profileleft.js
+++ components/Profileleft.js
@@ -8,13 +8,48 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { useEnsAvatar } from "wagmi";
+import { useEnsName } from "wagmi";
+import { useAccount } from "wagmi";
 
 const Profileleft = () => {
+  const { address: walletAdd } = useAccount();
+  const { data: ensName } = useEnsName();
+  const { data: ensAvatar } = useEnsAvatar();
+
+  function truncateLine(input) {
+    let line = typeof input === "number" ? input.toString() : input;
+    if (line.length > 8) {
+      return line.slice(0, 4) + "..." + line.slice(-4);
+    } else {
+      return line;
+    }
+  }
+
   return (
     <div className="top-0 left-0 w-3/12 borderr top-0 bottom-0 right-0">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Wallet Address: </CardTitle>
+      <Card className="w-[350px] h-[500px] mt-10 ">
+        <CardHeader className="my-6">
+          {ensAvatar ? (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              src={ensAvatar}
+              alt="avatar"
+              className="w-20 h-20 rounded-full mb-4"
+            />
+          ) : (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              src="/avatar.png"
+              alt="avatar"
+              className="w-20 h-20 rounded-full mb-4"
+            />
+          )}
+          {ensName ? (
+            <CardTitle>Wallet Address: {ensName}</CardTitle>
+          ) : (
+            <CardTitle>Wallet Address: {truncateLine(walletAdd)}</CardTitle>
+          )}
           <CardDescription>
             {/* replace with ens or pub key */}
             Deploy your new project in one-click.
